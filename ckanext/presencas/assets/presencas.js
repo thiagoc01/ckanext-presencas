e62a6fbc066dc3ff8f9e468cef522202feb412cf
Thiago Castro <thiago.castro99@hotmail.com>
2024-12-14 22:38:59 -0300

    Encerramento da utilização de proxyAll para uso do bind do JS
    De acordo com a documentação do JQuery, a função proxy foi descontinuada
    na versão 3.3. Ela recomenda o uso de bind, que é a função built-in do JavaScript
    que tem a mesma funcionalidade.

--- ckanext/presencas/assets/presencas.js
+++ ckanext/presencas/assets/presencas.js
@@ -4,102 +4,105 @@ ckan.module("presencas-data-inicio", function ($, _) {
 
     return {
 
-      options: {
-        debug: false,
-      },
+        options: {
+            debug: false,
+        },
 
-      initialize: function () {
-          $.proxyAll(this, /restauraMinMax|removeParametros|adicionaParametros/);
-          this.ESPACO_MAXIMO = 1;     // Máximo de um ano de diferença
-          this.sliderEsquerda = this.$('.slide:nth-of-type(1)');
-          this.sliderDireita = this.$('.slide:nth-of-type(2)');
-          this.intervaloSlide = this.$('.intervalo-slide');
-          this.valorMinimoSlider = this.sliderEsquerda.attr('min');
-          this.valorMaximoSlider = this.sliderEsquerda.attr('max');
+        initialize: function () {
+            this.restauraMinMax = this.restauraMinMax.bind(this);
+            this.removeParametros = this.removeParametros.bind(this);
+            this.adicionaParametros = this.adicionaParametros.bind(this);
+            this.ESPACO_MAXIMO = 1;     // Máximo de um ano de diferença
+            this.sliderEsquerda = this.$('.slide:nth-of-type(1)');
+            this.sliderDireita = this.$('.slide:nth-of-type(2)');
+            this.intervaloSlide = this.$('.intervalo-slide');
+            this.valorMinimoSlider = this.sliderEsquerda.attr('min');
+            this.valorMaximoSlider = this.sliderEsquerda.attr('max');
 
-          this.$('.slide:nth-of-type(1)').on('input', () => {
+            this.$('.slide:nth-of-type(1)').on('input', () => {
 
-              if (parseInt(this.sliderDireita.val()) - parseInt(this.sliderEsquerda.val()) <= this.ESPACO_MAXIMO)
-                  this.sliderEsquerda.val(parseInt(this.sliderDireita.val()) - this.ESPACO_MAXIMO);
+                if (parseInt(this.sliderDireita.val()) - parseInt(this.sliderEsquerda.val()) <= this.ESPACO_MAXIMO)
+                    this.sliderEsquerda.val(parseInt(this.sliderDireita.val()) - this.ESPACO_MAXIMO);
 
-              this.$('.slide1-val').first().text(this.sliderEsquerda.val());
+                this.$('.slide1-val').first().text(this.sliderEsquerda.val());
 
-              this.preencheIntervalosSliders();
-          });
+                this.preencheIntervalosSliders();
+            });
 
-          this.$('.slide:nth-of-type(2)').on('input', () => {
+            this.$('.slide:nth-of-type(2)').on('input', () => {
 
-              if (parseInt(this.sliderDireita.val()) - parseInt(this.sliderEsquerda.val()) <= this.ESPACO_MAXIMO)
-                  this.sliderDireita.val(parseInt(this.sliderEsquerda.val()) + this.ESPACO_MAXIMO);
+                if (parseInt(this.sliderDireita.val()) - parseInt(this.sliderEsquerda.val()) <= this.ESPACO_MAXIMO)
+                    this.sliderDireita.val(parseInt(this.sliderEsquerda.val()) + this.ESPACO_MAXIMO);
 
-              this.$('.slide2-val').first().text(this.sliderDireita.val());
+                this.$('.slide2-val').first().text(this.sliderDireita.val());
 
-              this.preencheIntervalosSliders();
-          });
+                this.preencheIntervalosSliders();
+            });
 
-          this.verificaExistenciaDataInicio();
-          this.sliderEsquerda.trigger('input');
-          this.sliderDireita.trigger('input');
-          this.$('button:nth-of-type(1)').on('click', this.restauraMinMax);
-          this.$('button:nth-of-type(2)').on('click', this.removeParametros);
-          this.$('button:nth-of-type(3)').on('click', this.adicionaParametros);
-      },
+            this.verificaExistenciaDataInicio();
+            this.sliderEsquerda.trigger('input');
+            this.sliderDireita.trigger('input');
+            this.$('button:nth-of-type(1)').on('click', this.restauraMinMax);
+            this.$('button:nth-of-type(2)').on('click', this.removeParametros);
+            this.$('button:nth-of-type(3)').on('click', this.adicionaParametros);
+            },
 
-      preencheIntervalosSliders:  function () {
+            preencheIntervalosSliders:  function () {
 
-          let porcentagemEsquerda = ( (this.sliderEsquerda.val() - this.valorMinimoSlider) / (this.valorMaximoSlider - this.valorMinimoSlider) ) * 100;
-          let porcentagemDireita = ( (this.sliderDireita.val() - this.valorMinimoSlider) / (this.valorMaximoSlider - this.valorMinimoSlider) ) * 100;
-          this.intervaloSlide.css('background', `linear-gradient(90deg, #e1e7f6 ${porcentagemEsquerda}% ,
-              #ef0614 ${porcentagemEsquerda}% , #ef0614 ${porcentagemDireita}%, #e1e7f6 ${porcentagemDireita}%)`);
-      },
+                let porcentagemEsquerda = ( (this.sliderEsquerda.val() - this.valorMinimoSlider) /
+                                        (this.valorMaximoSlider - this.valorMinimoSlider) ) * 100;
+                let porcentagemDireita = ( (this.sliderDireita.val() - this.valorMinimoSlider) /
+                                        (this.valorMaximoSlider - this.valorMinimoSlider) ) * 100;
+                this.intervaloSlide.css('background', `linear-gradient(90deg, #e1e7f6 ${porcentagemEsquerda}% ,
+                    #ef0614 ${porcentagemEsquerda}% , #ef0614 ${porcentagemDireita}%, #e1e7f6 ${porcentagemDireita}%)`);
+            },
 
-      verificaExistenciaDataInicio:  function () {
+            verificaExistenciaDataInicio:  function () {
 
-          const url = new URL(window.location.href);
+                const url = new URL(window.location.href);
 
-          if (url.searchParams.has('data_inicio'))
-          {
-              let data_inicio = url.searchParams.get('data_inicio');
+                if (url.searchParams.has('data_inicio'))
+                {
+                    let data_inicio = url.searchParams.get('data_inicio');
 
-              let [min, max] = data_inicio.match(/[0-9]+/g);
+                    let [min, max] = data_inicio.match(/[0-9]+/g);
 
-              this.sliderEsquerda.val(min);
-              this.sliderDireita.val(max);
-          }
-      },
+                    this.sliderEsquerda.val(min);
+                    this.sliderDireita.val(max);
+                }
+            },
 
-      restauraMinMax:  function () {
+            restauraMinMax:  function () {
 
-          let primeiroInput = this.$('.slide:nth-of-type(1)');
-          let segundoInput = this.$('.slide:nth-of-type(2)');
+                let primeiroInput = this.$('.slide:nth-of-type(1)');
+                let segundoInput = this.$('.slide:nth-of-type(2)');
 
-          primeiroInput.val(primeiroInput.attr('min'));
-          segundoInput.val(segundoInput.attr('max'));
+                primeiroInput.val(primeiroInput.attr('min'));
+                segundoInput.val(segundoInput.attr('max'));
 
-          this.sliderEsquerda.trigger('input');
-          this.sliderDireita.trigger('input');
-      },
+                this.sliderEsquerda.trigger('input');
+                this.sliderDireita.trigger('input');
+            },
 
-      removeParametros:  function () {
+            removeParametros:  function () {
 
-          const url = new URL(window.location.href);
+                const url = new URL(window.location.href);
 
-          if (url.searchParams.has('data_inicio'))
-          {
-              url.searchParams.delete('data_inicio');
+                if (url.searchParams.has('data_inicio'))
+                {
+                    url.searchParams.delete('data_inicio');
 
-              window.location = url;
-          }
-      },
+                    window.location = url;
+                }
+            },
 
-      adicionaParametros: function () {
+            adicionaParametros: function () {
 
-          const url = new URL(window.location.href);
+                const url = new URL(window.location.href);
 
-          url.searchParams.set('data_inicio', `[${this.sliderEsquerda.val()} TO ${this.sliderDireita.val()}]`);
+                url.searchParams.set('data_inicio', `[${this.sliderEsquerda.val()} TO ${this.sliderDireita.val()}]`);
 
-          window.location = url;
-      }
-    };
-
+                window.location = url;
+            }
+        };
 });
